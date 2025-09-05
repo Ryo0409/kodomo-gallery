ade7a4aed8b8505683b1bec29449e4b5ca04bd61
Ryo0409 <ryo04091762@gmail.com>
2025-09-06 00:09:22 +0900

    test: MessageModalのテストコード追加

---
+++ components/__tests__/MessageModal.test.tsx
@@ -0,0 +1,45 @@
+import { defaultConfig } from '@tamagui/config/v4'
+import { render, fireEvent } from '@testing-library/react-native'
+import React, { useState } from 'react'
+import { TamaguiProvider, createTamagui } from 'tamagui'
+
+import { themes } from '../../themes/themes'
+import MessageModal from '../MessageModal'
+
+const config = createTamagui({
+  ...defaultConfig,
+  themes,
+})
+
+function renderWithProvider(ui: React.ReactElement) {
+  return render(<TamaguiProvider config={config}>{ui}</TamaguiProvider>)
+}
+
+describe('MessageModal', () => {
+  it('MessageModalの表示', () => {
+    const onClose = jest.fn()
+    const { getByText } = renderWithProvider(
+      <MessageModal visible={true} message="テストメッセージ" onClose={onClose} />,
+    )
+    expect(getByText('テストメッセージ')).toBeTruthy()
+    expect(getByText('とじる')).toBeTruthy()
+  })
+
+  it('MessageModalの非表示', () => {
+    const onClose = jest.fn()
+    const { queryByText } = renderWithProvider(
+      <MessageModal visible={false} message="テストメッセージ" onClose={onClose} />,
+    )
+    expect(queryByText('テストメッセージ')).toBeNull()
+    expect(queryByText('とじる')).toBeNull()
+  })
+
+  it('MessageModalのonClose処理', () => {
+    const onClose = jest.fn()
+    const { getByText } = renderWithProvider(
+      <MessageModal visible={true} message="テストメッセージ" onClose={onClose} />,
+    )
+    fireEvent.press(getByText('とじる'))
+    expect(onClose).toHaveBeenCalled()
+  })
+})
